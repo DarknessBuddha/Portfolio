d0592ed1cffb7531b08cd608641a25488e1baf13
Ivan Lin <ilin@g.clemson.edu>
2022-12-31 18:29:32 -0500

    added button effect

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/Home.css'
 import useConveyor from "../hooks/useConveyor";
+import {motion, useAnimation} from "framer-motion";
 
 const Home = () => {
     const {Conveyor, conveyorController} = useConveyor()
+    const buttonController = useAnimation()
     return (
         <section className="home">
             <Conveyor />
@@ -13,8 +15,28 @@ const Home = () => {
                     <h4>Hello World!</h4>
                     <h1>I'm <span>DarkBuddha</span></h1>
                     <h2>I <span>build</span> things for <span>fun</span></h2>
-                    <button className="showcase-btn" onClick={conveyorController.start}>Gaze upon my power <i className="bi bi-rocket-takeoff"></i>
-                    </button>
+                    <motion.button className="showcase-btn"
+                                   onClick={()=>{
+                                       conveyorController.start()
+                                       buttonController.start("shrink").then(
+                                           ()=>buttonController.start("hide")
+                                       )
+                                   }}
+                                   animate={buttonController}
+                                   variants={{
+                                       shrink: {
+                                           width: 10,
+                                           height: 10,
+                                           contentVisibility: "hidden",
+                                           opacity: 0,
+                                       },
+                                       hide: {
+                                           visibility: "hidden"
+                                       }
+                                   }}
+                                   transition={{duration: 1}}>
+                        Gaze upon my power <i className="bi bi-rocket-takeoff"></i>
+                    </motion.button>
                 </div>
             </div>
         </section>
